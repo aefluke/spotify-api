06be734bb31bec647daeefb2ce9d39c122f2cf0a
SamiAkkaya <samiakkaya93@gmail.com>
2018-07-01 10:56:43 +0300

    index.js modified

--- index.js
+++ index.js
@@ -1,39 +1,31 @@
+var config = require('./config.json');
+var artist = new Object();
 var SpotifyWebApi = require('spotify-web-api-node');
-var spotifyInformation = require('./config.json');
-var spotifyApi = new SpotifyWebApi({
-    clientId: spotifyInformation.clientId,
-    clientSecret: spotifyInformation.clientSecret,
-    redirectUri: spotifyInformation.redirectUri
-});
-spotifyApi.setAccessToken(spotifyInformation.token);
+var spotifyApi = new SpotifyWebApi();
 
-let singerName = 'tarkan';
+spotifyApi.setAccessToken(config.accessToken);
 
-let getSingerPopularSongs = function () {
-    spotifyApi.search(singerName, ['artist'], {limit: 5}).then(
-        function (data) {
-            let singerId = data.body.artists.items[0].id;
-            console.log("artist image", data.body.artists.items[0].images[0].url);
-            console.log(singerName, 'ID = ', singerId);
-            spotifyApi.getArtistTopTracks(singerId, 'TR').then(
-                function (data) {
-                    for (var i = 0; i < 10; i++) {
-                        var popularSongList = [];
-                        popularSongList.push(data.body.tracks[i].preview_url);
-                        console.log(popularSongList);
-                        // console.log(singerName, ' Most Popular ' + i + ' Song = ', data.body.tracks[i].preview_url);
-                    }
-                    // console.log(singerName, ' Most Popular Song = ', data.body.tracks[0].preview_url);
-                },
-                function (err) {
-                    console.log('Singer does not found!!');
-                    console.error(err);
-                });
-        },
-        function (err) {
-            console.log('Singer does not found!!');
-            console.error(err);
+spotifyApi.searchArtists('Tarkan')
+    .then(function(data) {
+        artist = data.body.artists.items[0];
+        getTopTracks(artist.id);
+    }, function(err) {
+        console.error(err);
+    });
+
+function getTopTracks(id){
+    artist.tracks = [];
+    spotifyApi.getArtistTopTracks(id, 'TR')
+        .then(function(data) {
+            data.body.tracks.forEach(function(track) {
+                artist.tracks.push(track);
+                finish();
+            });
+        }, function(err) {
+            console.log('Something went wrong!', err);
         });
 }
-getSingerPopularSongs();
 
+function finish(){
+    console.log(artist);
+}
